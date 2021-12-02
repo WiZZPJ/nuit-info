99ea7b67d729b23a5d362f85969d61b0cde258a2
ShockedPlot7560 <66992287+ShockedPlot7560@users.noreply.github.com>
2021-12-03 00:11:04 +0100

    Ajout de multiple table et modi

--- migration/database.sql
+++ migration/database.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS `User` (
     `rank` INT(2) UNSIGNED NOT NULL DEFAULT '0',
     `token` VARCHAR(255) NOT NULL,
     `banned` BOOLEAN NOT NULL DEFAULT FALSE,
+    "verified" BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (`idUser`), 
     UNIQUE (`username`), 
     UNIQUE (`userEmail`)
@@ -20,13 +21,14 @@ CREATE TABLE IF NOT EXISTS `Article` (
     `articleAuthor` INT(11) NOT NULL,
     `accepted` BOOLEAN NOT NULL DEFAULT FALSE,
     `active` BOOLEAN NOT NULL,
+    "articleAuthor" INT(11) NOT NULL,
     PRIMARY KEY (`idArticle`),
     UNIQUE (`slugArticle`)
 ) ENGINE = InnoDB;
 
 ALTER TABLE `Article` 
     ADD CONSTRAINT `fk_Article_User`
-        FOREIGN KEY (`author`)  
+        FOREIGN KEY (`articleAuthor`)  
         REFERENCES `User` (`idUser`);
 
 CREATE TABLE IF NOT EXISTS `Media` (
@@ -47,20 +49,14 @@ CREATE TABLE IF NOT EXISTS `Category` (
     `idCategory` INT(11) NOT NULL AUTO_INCREMENT,
     `categoryName` VARCHAR(255) NOT NULL,
     `categoryDescription` VARCHAR(255) NOT NULL,
+    "parentCategory" INT(11),
     PRIMARY KEY (`idCategory`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `Rescue` (
-    `idRescue` INT(11) NOT NULL AUTO_INCREMENT,
-    `rescueDate` DATETIME NOT NULL,
-    `peopleSaved` INT(11) NOT NULL,
-    `detail` TEXT NOT NULL,
-    `source` JSON NOT NULL,
-    `crew` JSON NOT NULL,
-    `accepted` BOOLEAN DEFAULT FALSE,
-    `active` BOOLEAN,
-    PRIMARY KEY (`idRescue`)
-) ENGINE = InnoDB;
+ALTER TABLE "Category"
+    ADD CONSTRAINT "fk_Category_Category"
+        FOREIGN KEY ("parentCategory")
+        REFERENCES "Category" ("idCategory");
 
 CREATE TABLE IF NOT EXISTS `Boat` (
     `idBoat` INT(11) NOT NULL AUTO_INCREMENT,
@@ -73,17 +69,55 @@ CREATE TABLE IF NOT EXISTS `Boat` (
     UNIQUE (`nomBoat`)
 ) ENGINE = InnoDB;
 
+CREATE TABLE IF NOT EXISTS `Rescue` (
+    `idRescue` INT(11) NOT NULL AUTO_INCREMENT,
+    `rescueDate` DATETIME NOT NULL,
+    `peopleSaved` INT(11) NOT NULL,
+    `detail` TEXT NOT NULL,
+    `source` JSON NOT NULL,
+    `crew` JSON NOT NULL,
+    `accepted` BOOLEAN DEFAULT FALSE,
+    `active` BOOLEAN,
+    "boatUsed" INT(11) NOT NULL,
+    PRIMARY KEY (`idRescue`)
+) ENGINE = InnoDB;
+
+ALTER TABLE "Rescue"
+    ADD CONSTRAINT "fk_Rescue_Boat"
+        FOREIGN KEY ("boatUsed")
+        REFERENCES "Boat" ("idBoat");
+
 CREATE TABLE IF NOT EXISTS `Reward` (
     `idReward` INT(11) NOT NULL AUTO_INCREMENT,
     `entitled` VARCHAR(255) NOT NULL,
+    "aRescue" INT(11) NOT NULL,
     PRIMARY KEY (`idReward`)
 ) ENGINE = InnoDB;
+
+ALTER TABLE "Reward" 
+    ADD CONSTRAINT "fk_Reward_Rescue"
+        FOREIGN KEY ("aRescue")
+        REFERENCES "Rescue" ("idRescue");
 
 CREATE TABLE IF NOT EXISTS `Rescuer` (
     `firstName` VARCHAR(255) NOT NULL,
     `lastName` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`firstName`, `lastName`)
 ) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS "RescueRescuer" (
+    "aRescuer" INT(11) NOT NULL,
+    "aRescue" INT(11) NOT NULL,
+    PRIMARY KEY ("aRescuer", "aRescue")
+) ENGINE = InnoDB;
+
+ALTER TABLE "RescueRescuer"
+    ADD CONSTRAINT "fk_RescueRescuer_Rescuer"
+        FOREIGN KEY ("aRescuer")
+        REFERENCES "Rescuer" ("idRescuer"),
+    ADD CONSTRAINT "fk_RescueRescuer_Rescue"
+        FOREIGN KEY ("aRescue")
+        REFERENCES "Rescue" ("idRescue");
 
 CREATE TABLE IF NOT EXISTS `ArticleMedia` (
     `aArticle` INT(11) NOT NULL,
@@ -98,3 +132,31 @@ ALTER TABLE `ArticleMedia`
     ADD CONSTRAINT `fk_ArticleMedia_Media`
         FOREIGN KEY (`aMedia`)
         REFERENCES `Media` (`idMedia`);
+
+CREATE TABLE IF NOT EXISTS "ArticleCategory" (
+    "aArticle" INT(11) NOT NULL,
+    "aCategory" INT(11) NOT NULL,
+    PRIMARY KEY ("aArticle", "aCategory")
+) ENGINE  = InnoDB;
+
+ALTER TABLE "ArticleCategory" 
+    ADD CONSTRAINT "fk_ArticleCategory_Article"
+        FOREIGN KEY ("aArticle")
+        REFERENCES "Article" ("idArticle"),
+    ADD CONSTRAINT "fk_ArticleCategory_Category"
+        FOREIGN KEY ("aCategory")
+        REFERENCES "Category" ("idCategory");
+
+CREATE TABLE IF NOT EXISTS "RescuerReward" (
+    "aRescuer" INT(11) NOT NULL,
+    "aReward" INT(11) NOT NULL,
+    PRIMARY KEY ("aRescuer", "aReward");
+) ENGINE = InnoDB;
+
+ALTER TABLE "RescuerReward" 
+    ADD CONSTRAINT "fk_RescuerReward_Rescuer"
+        FOREIGN KEY ("aRescuer")
+        REFERENCES "Rescuer" ("idRescuer"),
+    ADD CONSTRAINT "fk_RescuerReward_Reward"
+        FOREIGN KEY ("aReward")
+        REFERENCES "Reward" ("idReward");
