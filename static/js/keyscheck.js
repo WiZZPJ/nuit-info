70910ac972eef943513780322c7abe7a615d5966
WiZZPJ <wizzpj@gmail.com>
2021-12-03 06:38:53 +0100

    .

--- static/js/keyscheck.js
+++ static/js/keyscheck.js
@@ -1,6 +1,7 @@
 const pressed = [];
 
 const secretCode = "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRight";
+const secretCode2 = "jesuisunelicorne";
 
 window.addEventListener("keyup", e => {
     pressed.push(e.key);
@@ -17,6 +18,23 @@ window.addEventListener("keyup", e => {
         } else {
             sessionStorage.setItem("dark", "false");
         }
+        pressed.length = 0;
+    }
+
+});
+
+window.addEventListener("keyup", e => {
+    pressed.push(e.key);
+    pressed.splice(
+        -secretCode2.length - 1,
+        pressed.length - secretCode2.length
+    );
+
+    if (pressed.join("").includes(secretCode2)) {
+        for(let i =0; i < 10; i++) {
+            alert("Ahah je te spam c'est drole");
+        }
+        
         pressed.length = 0;
     }
 
