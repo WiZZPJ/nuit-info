c36854c3c3fd970b398c467d54201a77166a679f
finxol <personnal@bottleneck.anonaddy.com>
2021-12-03 02:56:20 +0100

    add login.js

---
+++ static/js/login.js
@@ -0,0 +1,50 @@
+/**
+ * Create a dialog box in the web page to inform the user of activity
+ * @param {string} type Type of message (error, success, warning)
+ * @param {string} message Message to display
+ */
+function create_alert(type, message) {
+    let alertBanner = document.createElement('div');
+    alertBanner.classList.add("alert", type);
+    alertBanner.textContent = message;
+    document.body.appendChild(alertBanner);
+    let clear = setTimeout(() => {
+        document.body.removeChild(alertBanner);
+    }, 4000);
+    alertBanner.onclick = () => {
+        document.body.removeChild(alertBanner);
+        clearTimeout(clear);
+    };
+}
+
+
+
+let form = document.querySelector('form');
+form.onsubmit = (e) => {
+    e.preventDefault();
+    let data = {};
+    console.log(form);
+
+    form.querySelectorAll('input[name]').forEach(elem => {
+        data[elem.name] = elem.value;
+    });
+
+    let payload = new FormData();
+    payload.append( "json", JSON.stringify(data));
+
+    fetch('/api/v1/auth', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: payload
+    }).then(async data => {
+        console.log(data.status);
+        if (data.status === 200) {
+            create_alert('success', "Connexion réussie");
+        } else if (/(50*)|404/.test(String(data.status))) {
+            create_alert('error', "Identifiants incorrects")
+        }
+    });
+};
